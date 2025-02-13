a90c183a99df541886fd21a3e4de4f10c8bbb21b
MyohanMyolang <myohanmyolang@gmail.com>
2025-02-13 14:24:48 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+interface Some {}
+
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] place-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20"></div>
