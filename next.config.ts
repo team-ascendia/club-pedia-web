b2a123bf4992952b0451d3ca2ca5603d8a8e90d3
MyohanMyolang <myohanmyolang@gmail.com>
2025-02-16 17:41:54 +0900

    env 가져오는지 테스트

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import { config as dotenvConfig } from "dotenv"
 import type { NextConfig } from "next"
 dotenvConfig()
 
+console.log("env", process.env.HELLO)
+
 const nextConfig: NextConfig = {
   webpack: config => {
     config.module.rules.push({
