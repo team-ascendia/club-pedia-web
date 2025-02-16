4fc55dd135724ce119d41d9add958cb52928f5d7
김예린 <lynn1833@ewhain.net>
2025-02-17 01:20:48 +0900

    design: 버튼,헤더 공용 컴포넌트

---
+++ src/common/components/button/_next-button.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+
+interface NextButtonProps {
+  isActive?: boolean
+  nextPath?: string
+  onClick?: () => void
+  children: React.ReactNode
+}
+
+const NextButton: React.FC<NextButtonProps> = ({ isActive = false, nextPath, onClick, children }) => {
+  const router = useRouter()
+
+  const handleClick = () => {
+    if (isActive) {
+      if (onClick) {
+        onClick()
+      }
+      if (nextPath) {
+        router.push(nextPath)
+      }
+    }
+  }
+  return (
+    <button
+      disabled={!isActive}
+      onClick={handleClick}
+      className={`rounded-2 mb-14 flex w-full items-center justify-center px-6 py-[14px] text-base text-white 
+      ${isActive ? "bg-primary-400 " : "bg-gray-300"} 
+      transition-colors duration-300`}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default NextButton
