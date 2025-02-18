094ab25a10f12d4f80713508a2911c9f64722e72
김예린 <lynn1833@ewhain.net>
2025-02-18 10:58:52 +0900

    fix:msw 수정

---
+++ src/app/api/route.ts
@@ -0,0 +1,11 @@
+// export async function POST(request: Request) {
+//   return Response.json({
+//     accessToken: "mock_access_token",
+//     isSignup: false,
+//     name: "",
+//     phoneNumber: "",
+//     birthday: "1990-01-01",
+//     gender: "MALE",
+//     email: "hong@example.com",
+//   })
+// }
