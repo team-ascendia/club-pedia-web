3cd294b81f8a94c3e2441e2d54f31b913f5a2cfc
김예린 <lynn1833@ewhain.net>
2025-02-21 03:24:21 +0900

    design: padding 추가

--- src/app/(signup)/layout.tsx
+++ src/app/(signup)/layout.tsx
@@ -4,12 +4,12 @@ import { SignupProvider } from "@/src/domain/signup/context/signup-context"
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <>
+    <div className="px-6">
       <SignupProvider>
         <Script src="https://developers.kakao.com/sdk/js/kakao.js" strategy="beforeInteractive" />
         {children}
       </SignupProvider>
-    </>
+    </div>
   )
 }
 
