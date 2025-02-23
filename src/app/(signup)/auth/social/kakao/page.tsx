49b862b8a4454e7fda193a938fa31c452cf747fe
김예린 <lynn1833@ewhain.net>
2025-02-23 16:56:09 +0900

    chore: 소셜 로그인 페이지 구조 변경

--- src/app/(signup)/auth/social/kakao/page.tsx
+++
@@ -1,7 +0,0 @@
-import SocialLogin from "@/src/domain/oauth/social-login"
-
-const Page = () => {
-  return <SocialLogin socialType="kakao" />
-}
-
-export default Page
--- src/app/(signup)/auth/social/naver/page.tsx
+++
@@ -1,7 +0,0 @@
-import SocialLogin from "@/src/domain/oauth/social-login"
-
-const Page = () => {
-  return <SocialLogin socialType="naver" />
-}
-
-export default Page
