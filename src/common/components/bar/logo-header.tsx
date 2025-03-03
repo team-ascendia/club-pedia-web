a7e84be7e9248089777bce2733357fbdde853f81
김예린 <lynn1833@ewhain.net>
2025-03-03 18:52:23 +0900

    design: header,footer ui

--- src/common/components/bar/logo-header.tsx
+++ src/common/components/bar/logo-header.tsx
@@ -1,13 +1,16 @@
 "use client"
 
-import { Logo } from "@/public/icons"
+import { Logo, Notify, Search } from "@/public/icons"
 import cn from "@/src/common/util/cn"
 
 const LogoHeader = () => {
   return (
     <div className="flex items-center justify-between px-4 py-3">
       <Logo className={cn("w-28")} />
-      <div></div>
+      <div className="flex gap-x-4">
+        <Search className={cn("size-6")} />
+        <Notify className={cn("size-6")} />
+      </div>
     </div>
   )
 }
