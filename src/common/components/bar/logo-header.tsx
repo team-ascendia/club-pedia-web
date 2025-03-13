8deaf500197e163cb88edd036b31c19711f2939a
MyohanMyolang <myohanmyolang@gmail.com>
2025-03-13 20:26:39 +0900

    fix: 테스트용 버튼 제거

--- src/common/components/bar/logo-header.tsx
+++ src/common/components/bar/logo-header.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import Link from "next/link"
 import { Logo } from "@/public/icons"
 import cn from "@/src/common/util/cn"
 
@@ -8,7 +7,6 @@ const LogoHeader = () => {
   return (
     <div className="flex items-center justify-between px-4 py-3">
       <Logo className={cn("w-28")} />
-      <Link href="/club">로그인</Link>
     </div>
   )
 }
