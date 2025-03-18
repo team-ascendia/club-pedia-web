0bb0e770f018054d9fda32611c84cd620b66cee4
김예린 <lynn1833@ewhain.net>
2025-03-18 23:18:05 +0900

    fix:svg충돌 수정

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -1,15 +1,14 @@
 import ArrowLeftIcon from "./arrow-left.svg"
-import Heart from "./heart.svg"
-import Logo from "./logo.svg"
-import Sad from "./sad.svg"
 import CloseIcon from "./close.svg"
 import CommentIcon from "./comment.svg"
 import EventIcon from "./event.svg"
+import Heart from "./heart.svg"
 import HomeIcon from "./home.svg"
 import LocationIcon from "./location.svg"
 import Logo from "./logo.svg"
 import Notify from "./notification.svg"
 import RotateIcon from "./rotate.svg"
+import Sad from "./sad.svg"
 import Search from "./search.svg"
 import UserIcon from "./user.svg"
 
@@ -25,7 +24,6 @@ export {
   UserIcon,
   CommentIcon,
   RotateIcon,
-  Sad, 
-  Heart, 
-  Comment
+  Sad,
+  Heart,
 }
